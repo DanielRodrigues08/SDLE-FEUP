155597962d11a566f5c5abccc46b6fefa4e7bc80
Martim Videira <mitram790@gmail.com>
2023-11-08 15:31:09 +0000

    Built GSet crdt

---
+++ crdts/gset.js
@@ -0,0 +1,19 @@
+class GSet {
+    constructor() {
+        this.set = new Set();
+    }
+    add(element) {
+        this.set.add(element);
+    }
+    contains(element) {
+        return this.set.has(element);
+    }
+    elements() {
+        return Array.of(this.set.entries());
+    }
+    merge(other) {
+        for (const element of other.elements()) {
+            this.set.add(element);
+        }
+    }
+}
