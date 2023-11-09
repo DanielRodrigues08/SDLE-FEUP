74866a34bdc58ce9b941477a62b235b3953d0f7c
Martim Videira <mitram790@gmail.com>
2023-11-09 20:19:26 +0000

    Tests for causal context

--- crdts/test/test_causalcontext.js
+++ crdts/test/test_causalcontext.js
@@ -1,1 +1,19 @@
 import { CausalContext } from "../causalcontext";
+import { assertEquals } from "./testing";
+
+function test_next() {
+    const context = new CausalContext();
+    const [tag, count] = context.next("a");
+    assertEquals(tag, "a");
+    assertEquals(count, 1);
+}
+
+function test_max() {
+    const context = new CausalContext();
+    assertEquals(context.max("a"), 0);
+
+    for (let i = 0; i < 3; i++) {
+        context.next("a");
+    }
+    assertEquals(context.max("a"), 3);
+}
