8a0034f7f761bec7e29b04e6d27eb76aedb295d2
Martim Videira <mitram790@gmail.com>
2023-11-18 20:07:42 +0000

    Refactored awset merge function

--- crdts/awset.js
+++ crdts/awset.js
@@ -1,4 +1,30 @@
 import { CausalContext } from "./causalcontext.js";
+function setIntesection(a, b) {
+    let res = new Set();
+    for (const el of a.keys()) {
+        if (b.has(el)) {
+            res.add(el);
+        }
+    }
+    return res;
+}
+function setDifference(a, b) {
+    let intersection = setIntesection(a, b);
+    let res = new Set();
+    for (const el of a.keys()) {
+        if (!intersection.has(el)) {
+            res.add(el);
+        }
+    }
+    return res;
+}
+function safeGet(set, key, val) {
+    if (!set.has(key)) {
+        set.set(key, val);
+    }
+    return set.get(key);
+
+}
 
 class AWSet {
     constructor(tag) {
@@ -14,9 +40,10 @@ class AWSet {
         if (!this.items.has(element)) {
             this.items.set(element, new Set());
         }
-        this.items.get(element).add(contextItem);
+        this.items.get(element).add([this.tag, contextItem]);
     }
     remove(element) {
+
         if (!this.items.has(element)) {
             return;
         }
@@ -25,35 +52,12 @@ class AWSet {
     elements() {
         return this.items.keys();
     }
+    contains(element) {
+        return this.items.has(element);
+    }
+
     merge(other) {
 
-        debugger;
-        function setIntesection(a, b) {
-            let res = new Set();
-            for (const el of a.keys()) {
-                if (b.has(el)) {
-                    res.add(el);
-                }
-            }
-            return res;
-        }
-        function setDifference(a, b) {
-            let intersection = setIntesection(a, b);
-            let res = new Set();
-            for (const el of a.keys()) {
-                if (!intersection.has(el)) {
-                    res.add(el);
-                }
-            }
-            return res;
-        }
-        function safeGet(set, key, val) {
-            if (!set.has(key)) {
-                set.set(key, val);
-            }
-            return set.get(key);
-
-        }
         const allKeys = new Set();
         for (const key of this.items.keys()) { allKeys.add(key) };
         for (const key of other.items.keys()) { allKeys.add(key) };
@@ -71,22 +75,21 @@ class AWSet {
             for (const item of inCommon) {
                 result.add(item);
             }
-            // f filtering function
-            for (const item of inThis) {
-                // usar o seen.max(tag) ???
-                if (!other.seen.has(item)) {
-                    result.add(item);
-                }
-            }
-            // f filtering function
-            for (const item of inOther) {
-                // usar o seen.max(tag)??
-                if (!this.seen.has(item)) {
-                    result.add(item);
-                }
-            }
+
+            result.push(...this._filter(inThis, other.seen));
+            result.push(...this._filter(inOther, this.seen));
+            
             this.items.set(key, result);
         }
+    }
+    _filter(tags, causalcontext) {
+        let results = [];
+        for (const [tag, counter] of tags) {
+            if (causalcontext.seen.max(tag) < counter) {
+                results.push([tag, counter]);
+            }
+        }
+        return results;
     }
 
 }
