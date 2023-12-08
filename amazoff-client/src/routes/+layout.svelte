5f2384072e22ce117a69b65f4b635495bb4dda8c
Daniel Rodrigues <up202006562@up.pt>
2023-12-08 22:48:47 +0000

    added bootstrap

--- amazoff-client/src/routes/+layout.svelte
+++ amazoff-client/src/routes/+layout.svelte
@@ -1,14 +1,27 @@
 <script>
-    import { openedLists } from "./stores.js";
-    // This should be a a svelte store to be shared amonsgt other components
+  import { openedLists } from "./stores.js";
+  // This should be a a svelte store to be shared amongst other components
 </script>
 
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img
+        src="/logo.png"
+        alt="Logo"
+        class="d-inline-block align-text-top"
+      />
+      Amazoff
+    </a>
+  </div>
+</nav>
+
 <nav>
-    <a href="/">home</a>
-    <a href="/new-list">+</a>
-    {#each $openedLists.order as id}
-        <a href={`/list/${id}`}>{$openedLists.lists[id].name}</a>
-    {/each}
+  <a href="/">home</a>
+  <a href="/new-list">+</a>
+  {#each $openedLists.order as id}
+    <a href={`/list/${id}`}>{$openedLists.lists[id].name}</a>
+  {/each}
 </nav>
 
 <slot />
