1b17b3711e2fdd264c9f6b3938e5482fb2b30279
Martim Videira <mitram790@gmail.com>
2023-12-09 22:54:25 +0000

    Adding New Items though the UI

--- amazoff-client/src/routes/list/[id]/+page.svelte
+++ amazoff-client/src/routes/list/[id]/+page.svelte
@@ -6,12 +6,37 @@
   export let data;
   $: if ($navigating) openedLists.setCurrent(data.id);
 
+  let newItem = {
+    name: "",
+    desired: 1,
+    purchased: 0,
+  };
+
   function closeList() {
     goto("/");
     openedLists.close(data.id);
   }
 
   let list;
+
+  function addNewItem() {
+    newItem.name = newItem.name.trim();
+    if (newItem.name === "") {
+      return;
+    }
+    list.addItem(newItem.name);
+    list.items.get(newItem.name).get("desired").increment(newItem.desired);
+    list.items.get(newItem.name).get("purchased").increment(newItem.purchased);
+    const myModal = document.getElementById("closeModalDoDani")?.click();
+
+    // Magic Svelte Sama
+    list = list;
+  }
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      addNewItem();
+    }
+  }
   let items;
   $: {
     if (data.id === $openedLists.current && $openedLists.lists[data.id]) {
@@ -46,6 +71,7 @@
       <div class="modal-header">
         <h1 class="modal-title fs-5">Add item</h1>
         <button
+          id="closeModalDoDani"
           type="button"
           class="btn-close"
           data-bs-dismiss="modal"
@@ -61,6 +87,8 @@
               class="form-control"
               id="name"
               placeholder="Enter item name"
+              bind:value={newItem.name}
+              on:keydown={(e) => handleKeyDown(e)}
             />
           </div>
           <div class="mb-3">
@@ -71,6 +99,8 @@
               id="quantity"
               placeholder="Desired quantity"
               min="1"
+              bind:value={newItem.desired}
+              on:keydown={(e) => handleKeyDown(e)}
             />
           </div>
           <div class="mb-3">
@@ -81,6 +111,8 @@
               id="purchased"
               placeholder="Purchased quantity"
               min="0"
+              bind:value={newItem.purchased}
+              on:keydown={(e) => handleKeyDown(e)}
             />
           </div>
         </form>
@@ -89,7 +121,9 @@
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
           >Close</button
         >
-        <button type="button" class="btn btn-primary">Add</button>
+        <button type="button" class="btn btn-primary" on:click={addNewItem}
+          >Add</button
+        >
       </div>
     </div>
   </div>
